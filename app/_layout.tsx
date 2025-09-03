0e1744d4ff218d43dbdbe689e5ab9416cddb18da
gabethemyers <gabethemyers@gmail.com>
2025-09-03 11:43:29 -0700

    added initial database setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import LoginScreen from "./login";
+import { initDB } from "./db/database";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    initDB();
+  },[]); 
   return <LoginScreen/>;
-
 }
