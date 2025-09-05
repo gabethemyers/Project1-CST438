9e25bb02462906ee2839f50828e50379270593f2
JZavala210 <juanczavala22@gmail.com>
2025-09-05 16:35:21 -0700

    Fixed layout of files and prepared logic for login page once the datbase is created

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,6 @@ export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="landingPage" options={{ headerTitle: 'Home' }} />
-      <Tabs.Screen name="cardsPage" options={{ headerTitle: 'Cards' }} />
       <Tabs.Screen name="index" options={{ href: null }} />
     </Tabs>
   );
