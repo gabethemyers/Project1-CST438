3501de1fac1db72dd485e3967048ac1c116334d0
gabethemyers <gabethemyers@gmail.com>
2025-09-15 14:44:34 -0700

    added tables for deck and card storage locally. Added tests for new functions

--- db/connection.ts
+++ db/connection.ts
@@ -21,9 +21,29 @@ export async function getDBConnection() {
         username TEXT NOT NULL,
         password TEXT NOT NULL
       );
-      CREATE TABLE IF NOT EXISTS api_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        data TEXT NOT NULL
+      CREATE TABLE IF NOT EXISTS decks (
+        deck_id INTEGER PRIMARY KEY NOT NULL,
+        user_id INTEGER,
+        name TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+      );
+      CREATE TABLE IF NOT EXISTS cards (
+        card_id INTEGER PRIMARY KEY NOT NULL,
+        name TEXT,
+        rarity TEXT,
+        elixir_cost INTEGER,
+        max_level INTEGER,
+        max_evolution INTEGER,
+        icon_url_medium TEXT,
+        icon_url_large TEXT
+      );
+      CREATE TABLE IF NOT EXISTS deck_cards (
+        deck_id INTEGER,
+        card_id INTEGER,
+        slot_number INTEGER,
+        PRIMARY KEY (deck_id, slot_number),
+        FOREIGN KEY (deck_id) REFERENCES decks(deck_id),
+        FOREIGN KEY (card_id) REFERENCES cards(card_id)
       );
     `);
     
