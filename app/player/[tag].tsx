79c3c774d06580c2dd29b0475cc8fc17daf4a757
JZavala210 <juanczavala22@gmail.com>
2025-09-26 20:24:40 -0700

    Clean up tag.tsx duplciate code

--- app/player/[tag].tsx
+++ app/player/[tag].tsx
@@ -25,11 +25,7 @@ type Player = {
   currentDeck?: Card[];
 };
 
-
 const apiKey = process.env.EXPO_PUBLIC_CLASH_ROYALE_API_KEY;
-
-const apiKey =  process.env.EXPO_PUBLIC_CLASH_ROYALE_API_KEY;
-
 
 export default function PlayerScreen() {
   const { tag: rawTag } = useLocalSearchParams<{ tag: string }>();
